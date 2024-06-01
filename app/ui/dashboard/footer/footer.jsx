2c89baabbbdc2463f358f29337a777f22298e574
SuisF <enykura394@gmail.com>
2024-06-01 15:45:15 +0700

    first commit

---
+++ app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./footer.module.css"
+
+const Footer = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.logo}>Fardhan Hafiz R</div>
+            <div className={styles.text}>™ All Rights Reserved.</div>
+        </div>
+    )
+}
+      
+export default Footer
